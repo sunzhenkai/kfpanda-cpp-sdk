159bc89b6a3006791e468aae97bc18b0f333091a
zhenkai.sun <zhenkai.sun@qq.com>
2025-07-19 10:39:58 +0800

    decrease connection timeout

--- kfpanda/client/client.cc
+++ kfpanda/client/client.cc
@@ -15,8 +15,8 @@ absl::Status KfpandaClient::Init() {
   brpc::ChannelOptions options;
   options.protocol = "baidu_std";
   options.connection_type = "single";
-  options.connect_timeout_ms = 3000;
-  options.timeout_ms = 3000 /*milliseconds*/;
+  options.connect_timeout_ms = 1000;
+  options.timeout_ms = 1000 /*milliseconds*/;
   options.max_retry = 0;
   if (channel_.Init(server_.c_str(), "", &options) != 0) {
     has_error_ = true;
